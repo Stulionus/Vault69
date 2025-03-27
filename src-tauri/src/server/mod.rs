bb77c97afa2bdce199602c146f7f02dc36c43268
maxmalkin <maxim_malkin@outlook.com>
2025-03-26 20:49:36 -0700

    this doesnt build lol

--- src-tauri/src/server/mod.rs
+++ src-tauri/src/server/mod.rs
@@ -8,13 +8,14 @@ use tauri::AppHandle;
 struct TauriAppState {
 	app: Mutex<AppHandle>
 }
+
 #[actix_web::main]
 pub async fn init(app: AppHandle) -> std::io::Result<()> {
 	let tauri_app = web::Data::new(TauriAppState {
 		app: Mutex::new(app),
 	});
 
-	HttpServer:new(move || {
+	HttpServer::new(move || {
 		App::new()
 		.app_data(tauri_app.clone())
 		.wrap(middleware::Logger::default())
