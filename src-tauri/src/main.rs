549d596daf716d3a522c49d7a232f381a7b937ad
Max Malkin <maxim_malkin@outlook.com>
2025-04-17 16:06:57 -0600

    fml

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,7 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-mod server;
-use std::thread;
 
 fn main() {
-    my_tauri_app_lib::run();
-
     tauri::Builder::default()
-    .setup(|app| {
-        let handle = app.handle();
-        let boxed_handle = Box::new(handle);
-
-        thread::spawn(move || {
-            server::init(*boxed_handle).unwrap();
-        });
-        Ok(());
-    })
-    .invoke_handler(tauri::generate_handle![greet])
-    .run(tauri::generate_context!())
-    .expect("Error running tauri application");
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
